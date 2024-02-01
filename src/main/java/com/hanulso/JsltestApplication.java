43b311a4902fb2c2e14c9ec53b8dc81ddfa3d7a0
Park-Boyoung <bypark9411@gmail.com>
2024-02-01 18:09:44 +0900

    feat : jsltest

---
+++ src/main/java/com/hanulso/JsltestApplication.java
@@ -0,0 +1,13 @@
+package com.hanulso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsltestApplication.class, args);
+	}
+
+}
